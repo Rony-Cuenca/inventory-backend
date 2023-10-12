f1f080f97ccf8dfcc4e5bab6c6f00f2bee173750
Rony-Cuenca <JRony.Cr@gmail.com>
2023-10-12 18:28:41 -0500

    eliminar categoria

--- src/main/java/com/companyjr/inventory/controller/CategoryRestController.java
+++ src/main/java/com/companyjr/inventory/controller/CategoryRestController.java
@@ -2,6 +2,7 @@ package com.companyjr.inventory.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,28 +22,59 @@ public class CategoryRestController {
 	@Autowired
 	private ICategoryService service;
 	
+	/**
+	 * 
+	 * @return
+	 */
 	/*get all the categories*/
 	@GetMapping("/categories")
 	public ResponseEntity<CategoryResponseRest> searchCategories(){
 		ResponseEntity<CategoryResponseRest> response =  service.search();
 		return response;
 	}
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 */
 	/*get categories by id*/
 	@GetMapping("/categories/{id}")
 	public ResponseEntity<CategoryResponseRest> searchCategoriesById(@PathVariable Long id){
 		ResponseEntity<CategoryResponseRest> response =  service.searchById(id);
 		return response;
 	}
+	/**
+	 * 
+	 * @param category
+	 * @return
+	 */
 	/*save categories by id*/
 	@PostMapping("/categories")
 	public ResponseEntity<CategoryResponseRest> save(@RequestBody Category category){
 		ResponseEntity<CategoryResponseRest> response =  service.save(category);
 		return response;
 	}
-	/*update categories by id*/
+	/**
+	 * 
+	 * @param category
+	 * @param id
+	 * @return
+	 */
+	/*update categories*/
 	@PutMapping("/categories/{id}")
 	public ResponseEntity<CategoryResponseRest> update(@RequestBody Category category, @PathVariable Long id){
 		ResponseEntity<CategoryResponseRest> response =  service.update(category, id);
+		return response;
+	}
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 */
+	/*delete categories*/
+	@DeleteMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> delete(@PathVariable Long id){
+		ResponseEntity<CategoryResponseRest> response =  service.deleteById(id);
 		return response;
 	}
 
