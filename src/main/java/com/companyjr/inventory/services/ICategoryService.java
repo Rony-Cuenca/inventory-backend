0001371cfd750737050a7381a9e647ed62db176f
Rony-Cuenca <JRony.Cr@gmail.com>
2023-10-12 17:50:01 -0500

    servicio guardar categoria

--- src/main/java/com/companyjr/inventory/services/ICategoryService.java
+++ src/main/java/com/companyjr/inventory/services/ICategoryService.java
@@ -2,11 +2,13 @@ package com.companyjr.inventory.services;
 
 import org.springframework.http.ResponseEntity;
 
+import com.companyjr.inventory.model.Category;
 import com.companyjr.inventory.response.CategoryResponseRest;
 
 public interface ICategoryService {
 	
 	public ResponseEntity<CategoryResponseRest> search();
 	public ResponseEntity<CategoryResponseRest> searchById(Long id);
+	public ResponseEntity<CategoryResponseRest> save(Category category);
 
 }
