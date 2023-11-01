7fabb45ef8ec8aa9def22ab3b2a6b6e63ac810cd
Rony-Cuenca <JRony.Cr@gmail.com>
2023-10-31 19:58:57 -0500

    LISTAR PRODUCTOS

--- src/main/java/com/companyjr/inventory/controller/ProductRestController.java
+++ src/main/java/com/companyjr/inventory/controller/ProductRestController.java
@@ -88,4 +88,13 @@ public class ProductRestController {
 		ResponseEntity<ProductResponseRest> response = productService.deleteById(id);
 		return response;
 	}
+	/**
+	 * search products
+	 * @return
+	 */
+	@GetMapping("/products")
+	public ResponseEntity<ProductResponseRest> search(){
+		ResponseEntity<ProductResponseRest> response = productService.search();
+		return response;
+	}
 }
